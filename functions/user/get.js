e9413453affede563f46b8387eb5f465269e05a3
Johnny Moreira <jmoreira@engebras.com.br>
2022-01-27 04:17:29 -0300

    fix(main): add getUser and prisma

---
+++ functions/user/get.js
@@ -0,0 +1,17 @@
+const prisma = require('../../prisma/index');
+
+module.exports.getUser = async (event) => {
+
+    let user = await prisma.user.findMany()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          user
+        },
+        null,
+        2
+      ),
+    };
+};
